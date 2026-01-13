9ca1b446004c41eebf6d7bcdff0759d3c11aac1e
saicharan0977-rgb <saicharan0977@gmail.com>
2026-01-13 05:54:32 +0000

    Have added all the containers for dom objects

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+const input = document.querySelector(".choice");
+const red = document.querySelector(".red");
+const black = document.querySelector(".black");
+const rgb = document.querySelector(".red");
+const blue = document.querySelector(".blue");
+const board = document.querySelector(".design");
